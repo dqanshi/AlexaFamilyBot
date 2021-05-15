a770ed901c0a7c5ca85467477b84aed8bbd951a3
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-05-15 23:21:35 +0530

    Create panel_string.py

---
+++ haruka/modules/helper_funcs/panel_string.py
@@ -0,0 +1,27 @@
+from tg_bot import dispatcher, INTRO_IMG
+
+if INTRO_IMG is None:
+    HYPER_X_IMG = "https://telegra.ph/file/e1e804f82a68529627555.jpg"
+else:
+  HYPER_X_IMG = INTRO_IMG    
+SOURCE_STRING = """
+✯Im built-in python 3 to help and manage groups
+✯To clone me [𝘾𝙡𝙞𝙘𝙠 𝙝𝙚𝙧𝙚](https://github.com)
+✯For bot updates [𝙅𝙤𝙞𝙣 𝙃𝙚𝙧𝙚](https://t.me/)
+"""
+
+HELP = """
+𝑯𝒆𝒚 𝑰𝒂𝒎  *{}*.
+I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
+the things I can help you with
+✰𝙈𝙖𝙞𝙣 𝙖𝙫𝙖𝙞𝙡𝙖𝙗𝙡𝙚 𝙘𝙤𝙢𝙢𝙖𝙣𝙙𝙨✰:
+ ➪ /start: start the bot
+ ➪ /help: PM's you this message.
+ ➪ /help <module name>: PM's you info about that module.
+ ➪ /source: Information about my source.
+ ➪ /settings:
+  » in PM: will send you your settings for all supported modules.
+  » in a group: will redirect you to pm, with all that chat's settings.
+{}
+And the following:
+"""
